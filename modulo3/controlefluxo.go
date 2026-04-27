b86713e4656247c6c2a44faed1e57606945cf651
Breno Silva <brenosill@hotmail.com>
2026-04-27 13:17:15 -0300

    feat: Adicionado verificação

--- modulo3/controlefluxo.go
+++ modulo3/controlefluxo.go
@@ -2,8 +2,19 @@ package modulo3
 
 import (
 	"fmt"
+	"github.com/badoux/checkmail"
 )
 
 func ControleFluxo() {
 	fmt.Println("=== CONTROLE DE FLUXO ===")
+
+	err := checkmail.ValidateFormat("teste@teste.com")
+	
+	if err != nil {
+		fmt.Println("Email inválido")
+	} else {
+		fmt.Println("Email válido")
+	}
+
+
 }
